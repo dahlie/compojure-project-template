ca6c804378503e551afc81055b4bc4a99cef4fa6
Hannu <hannu.pousi@leonidasoy.fi>
2015-09-26 22:17:38 +0300

    Update context.clj

--- src/template/context.clj
+++ src/template/context.clj
@@ -10,8 +10,7 @@
 (defmethod compojure.api.meta/restructure-param :context
   [_ sym acc]
   (update-in acc [:lets] into [sym `{:db            (-> ~'+compojure-api-request+ h/get-components :db :db)
-                                     :env           (-> ~'+compojure-api-request+ h/get-components :env)
-                                     :session       (delay ((-> ~'+compojure-api-request+ h/get-components :tweb :session)))}]))
+                                     :env           (-> ~'+compojure-api-request+ h/get-components :env)}]))
 
 (defn system->context [system & [user]]
   {:db (get-in system [:db :db])
